b7fefa3fb24ceaae0b01be690e3f86e1680cb2a2
Mantas Gelčius <manciusg@gmail.com>
2017-03-09 16:33:34 +0200

    Added function for generating index.html

--- src/app/console/commands/HCDocs.php
+++ src/app/console/commands/HCDocs.php
@@ -5,6 +5,7 @@ use Go\ParserReflection\ReflectionClass;
 use Go\ParserReflection\ReflectionMethod;
 use interactivesolutions\honeycombcore\commands\HCCommand;
 use Nette\Reflection\AnnotationsParser;
+use phpDocumentor\Reflection\File;
 use Symfony\Component\Finder\Finder;
 
 class HCDocs extends HCCommand
@@ -89,7 +90,36 @@ class HCDocs extends HCCommand
         }
 
         $this->createDocFile($classesInfo);
+        $this->createIndexFile();
     }
+
+    public function createIndexFile()
+    {
+        $dir = base_path() . "/public/docs/";
+        $files = scandir($dir);
+        $template = $this->file->get(__DIR__ . '/templates/docs/packageBlock.hctpl');
+        $output = '';
+
+        foreach ($files as $file)
+            if (substr($file, -5) == ".html" && $file != "index.html")
+            {
+                $fileName = (explode('-',$file));
+                unset($fileName[0]);
+                $fileName = substr(implode(' ', $fileName),0 , -5);
+                $field = str_replace('{packageName}', ucfirst($fileName), $template);
+                $field = str_replace('{file}', $file, $field);
+                $output .= $field;
+            }
+
+        $this->createFileFromTemplate([
+            "destination"         => base_path('public/docs/index.html'),
+            "templateDestination" => __DIR__ . '/templates/docs/index.hctpl',
+            "content"             => [
+                "packageBlock" => $output
+            ],
+        ]);
+    }
+
 
     /**
      * @param $classesInfo
@@ -327,6 +357,10 @@ class HCDocs extends HCCommand
         return $output;
     }
 
+    /**
+     * @param $classesInfo
+     * @return string
+     */
     public function createCommandsMenu($classesInfo)
     {
         $file = $this->file->get(__DIR__ . '/templates/docs/commandsMenu.hctpl');
